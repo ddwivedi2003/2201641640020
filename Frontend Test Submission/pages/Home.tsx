5039f49c084ef9bd7fb1376918c17d5b53ef1659
Dewashish Dwivedi <135425359+ddwivedi2003@users.noreply.github.com>
2025-09-08 14:00:06 +0530

    created Home.tsx

---
+++ Frontend Test Submission/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import UrlShortenerForm from '../components/UrlShortenerForm';
+import UrlList from '../components/UrlList';
+
+const Home: React.FC = () => {
+  return (
+    <Box>
+      <Typography variant="h5" mb={2}>
+        Affordmed URL Shortener
+      </Typography>
+      <UrlShortenerForm />
+      <Box mt={3}>
+        <UrlList />
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
