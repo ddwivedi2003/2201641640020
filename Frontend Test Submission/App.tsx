f5171573c2c7613544fe807fbbb9e0d24f7362a2
Dewashish Dwivedi <135425359+ddwivedi2003@users.noreply.github.com>
2025-09-08 13:56:59 +0530

    created App.tsx

---
+++ Frontend Test Submission/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container, CssBaseline, AppBar, Toolbar, Button } from '@mui/material';
+import Home from './pages/Home';
+import Statistics from './pages/Statistics';
+import ShortUrlRedirect from './components/ShortUrlRedirect';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" href="/">Shortener</Button>
+          <Button color="inherit" href="/stats">Statistics</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stats" element={<Statistics />} />
+          <Route path="/:shortcode" element={<ShortUrlRedirect />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
+};
+
+export default App;
